10eca5e9a83168a6ac84701d5d55a99a8e3a3a48
Venkataramanasam Sambath <svenkataramanasam@svenkataramanasam-MBP15.local>
2021-07-01 11:48:52 +0530

    Initial commit

---
+++ src/main/java/ApacheHive.java
@@ -0,0 +1,32 @@
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class ApacheHive
+{
+    public static void main(final String[] args) throws SQLException {
+        try {
+            Class.forName("org.apache.hive.jdbc.HiveDriver");
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        final Connection con = DriverManager.getConnection("jdbc:hive2://host-10-17-102-106.coe.cloudera.com:10000/;principal=hive/_HOST@C6CITIDSE.COM");
+        final Statement stmt = con.createStatement();
+        stmt.setFetchSize(10000);
+
+        ResultSet res = stmt.executeQuery("select * from airline_data.airlines_1million limit 370000");
+        long time_1 = System.currentTimeMillis();
+        while (res.next()) {
+     //       System.out.println(res.getString(1));
+        }
+        long time_2 = System.currentTimeMillis();
+        long difference = time_1 - time_2;
+        System.out.println( difference + "milliseconds" );
+
+    }
+}
